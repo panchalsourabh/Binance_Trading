9ee99b78827b45462231f88d9437f979ec7cefe3
panchalsourabh <151991787+panchalsourabh@users.noreply.github.com>
2026-02-26 13:08:50 +0530

    cli_app.py

--- Binanace_trading/cli_app.py
+++ Binanace_trading/cli_app.py
@@ -12,7 +12,7 @@ def main(
     quantity: float = typer.Option(..., "--quantity"),
     price: Optional[float] = typer.Option(None, "--price")
 ):
-    # Use your actual Testnet API keys here
+    
     client = BinanceFuturesClient("dP6HCAXQukrET6Of0PRVaJQ2C77xE9XIKEQVj5ptBWF78hoRAiOTiPI3bOqGO6Gh", "06Y6Hn1PW4ywnPnmHX2LfZoQKTKje2v7sBW1YVP3zugHOhzieRxA5oAqE0XqxQ29")
     
     try:
@@ -26,11 +26,8 @@ def main(
         print(f"Side:         {res.get('side')}")
         print(f"Type:         {res.get('type')}")
         
-        # For Market orders, executedQty is what you want to see
         print(f"Qty Ordered:  {res.get('origQty')}")
         
-        # If the order filled instantly, these might still be 0 in the initial response
-        # but you can check them like this:
         avg_price = res.get('avgPrice') if float(res.get('avgPrice', 0)) > 0 else "Market Price"
         print(f"Avg Price:    {avg_price}")
         print("-" * 30)
@@ -39,4 +36,5 @@ def main(
         print(f"❌ Order Failed: {e}")
               
 if __name__ == "__main__":
+
     app()
